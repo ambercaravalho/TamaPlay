f51eeb6adbb9a8a01931a7ad2b9310566d195b90
ambercaravalho <equable_microbe.0a@icloud.com>
2024-04-16 22:02:08 -0400

    Update .gitignore to exclude img/*~ and modify main.lua to load gen1_a_bg.png as the background image

--- Source/main.lua
+++ Source/main.lua
@@ -1,28 +1,27 @@
 import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 
--- Get a shorter alias for the graphics module
+-- alias for graphics module
 local gfx = playdate.graphics
 
--- Load the PNG image
-local image = gfx.image.new("img/gen1_bg.png")
+-- load image
+local image = gfx.image.new("img/gen1_a_bg.png")
 
--- Assert to ensure the image is loaded correctly
+-- ensure image is loaded correctly
 assert(image)
 
--- Function to set up the game environment
-function setup()
-    -- Set the image as the background
+-- set up game environment
+local function setup()
+    -- set the image as the background
     gfx.sprite.setBackgroundDrawingCallback(function(x, y, width, height)
-        -- Draw the image at position (0, 0)
         image:draw(0, 0)
     end)
 end
 
--- Call setup to initialize the game environment
+-- init game environment
 setup()
 
--- The main update loop
+-- main update loop
 function playdate.update()
     gfx.sprite.update()
 end
